c5567a486bdb2a8f831658185f852f433ab4c57a
wst3050469 <108894205@qq.com>
2026-06-21 12:18:07 +0800

    feat: add AI marketing system v5.0.0

---
+++ docs/marketing_system.py
@@ -0,0 +1,13 @@
+# Yongyi Marketing System v5.0.0
+import json
+from datetime import datetime
+class KE:
+ @staticmethod
+ def get_kws():
+  return [{"w":"epoxy"},{"w":"inorganic"}]
+class CA:
+ @staticmethod
+ def get_comps():
+  return [{"n":"CompA"}]
+if __name__=="__main__":
+ print("Ready")
